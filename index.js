90f9f58c2473f9b9e9f7dadfab54a759495964ae
CelesteSMThomas <celestesmthomas@gmail.com>
2021-02-17 22:09:15 -0800

    commit tree 9eb55d22581c9fcd97284d273a0563472917b670

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+let messages = [];
+function writeCards(array, name) {
+  for (let i = 0; i < array.length; i++) {
+    messages.push(`Thank you, ${array[i]}, for the wonderful ${name} gift!`);
+  }
+  return messages;
+}
+
+function countDown(number) {
+  while (number + 1 > 0) {
+    console.log(number--);
+  }
+}
